04664d30739378f9b2aec71093b631d8e48e77b5
Zayda Fernanda Vargas <zaydafervar@gmail.com>
2026-02-05 19:49:10 -0600

    Configuración de CRUD en Task

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
-  @Get('tasks')
-  login() {
-    return 'Tareas Completadas';
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  getTasks() {
+    return this.taskService.getTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: number) {
+    return this.taskService.getTasksById(Number(id));
+  }
+
+  @Post()
+  insertTask(@Body() task: any) {
+    return this.taskService.insert(task);
+  }
+
+  @Put(':id')
+  updateTask(@Param('id') id: number, @Body() task: any) {
+    return this.taskService.update(Number(id), task);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: number) {
+    return this.taskService.delete(Number(id));
   }
 }
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TaskService {
-  login(): string {
-    return 'Tarea Acabada';
+  public getTasks() {
+    return 'Lista de tareas';
+  }
+
+  public getTasksById(id: number): string {
+    return `Tarea con ID ${id}`;
+  }
+
+  public insert(task: any): string {
+    return `Tarea insertada: ${task}`;
+  }
+
+  public update(id: number, task: any) {
+    return `Tarea con ID ${id} actualizada con ${task}`;
+  }
+
+  public delete(id: number): string {
+    return `Tarea con ID ${id} eliminada`;
   }
 }
